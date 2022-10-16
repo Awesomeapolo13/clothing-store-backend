6449ac05313a98fd7f2422e082c5d7dd4b39290a
ALogachev <aelog16@yandex.ru>
2022-10-16 19:50:16 +0300

    Task-2. PHPUnit and Symfony test packs were installed. The one test example was added

---
+++ tests/Functional/Controller/HealthCheckActionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Functional\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class HealthCheckActionTest extends WebTestCase
+{
+    public function test_request_responded_successful_result(): void
+    {
+        $client = static::createClient();
+        $client->request(Request::METHOD_GET, '/health-check');
+        // Проверяет, является ли запрос успешным
+        $this->assertResponseIsSuccessful();
+        // Определим возвращает ли ответ json со статусом ok
+        $jsonResult = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('ok', $jsonResult['status']);
+    }
+}
