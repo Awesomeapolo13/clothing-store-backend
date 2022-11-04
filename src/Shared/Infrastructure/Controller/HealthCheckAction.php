48db6ee5a4c63751c446fbe01d7841f3b4018e55
ALogachev <aelog16@yandex.ru>
2022-11-04 17:48:51 +0300

    Task-3. The Monolith first architecture was built

--- src/Shared/Infrastructure/Controller/HealthCheckAction.php
+++ src/Shared/Infrastructure/Controller/HealthCheckAction.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-namespace Controller;
+namespace App\Shared\Infrastructure\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
