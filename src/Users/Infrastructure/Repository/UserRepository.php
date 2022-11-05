cd396d421644ae15a3b805265cee5dd869b4435e
ALogachev <aelog16@yandex.ru>
2022-11-05 17:25:03 +0300

    Task-3. The test for the user create case was added

--- src/Users/Infrastructure/Repository/UserRepository.php
+++ src/Users/Infrastructure/Repository/UserRepository.php
@@ -2,11 +2,33 @@
 
 declare(strict_types=1);
 
-namespace Users\Infrastructure\Repository;
+namespace App\Users\Infrastructure\Repository;
 
+use App\Users\Domain\Entity\User;
 use App\Users\Domain\Repository\UserRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class UserRepository implements UserRepositoryInterface
+class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
 
+    public function add(User $user): void
+    {
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    public function findByUlid(string $ulid): ?User
+    {
+        return $this->find($ulid);
+    }
+
+    public function findByEmail(string $email): ?User
+    {
+        return $this->findOneBy(['email' => $email]);
+    }
 }
