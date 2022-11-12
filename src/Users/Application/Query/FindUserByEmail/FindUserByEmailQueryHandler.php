c65165e1ebcf3c7baada678cc20486d01d7210d7
ALogachev <aelog16@yandex.ru>
2022-11-12 13:16:36 +0300

    Task-4. Realized queries and commands handlers

---
+++ src/Users/Application/Query/FindUserByEmail/FindUserByEmailQueryHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Users\Application\Query\FindUserByEmail;
+
+use App\Shared\Application\Query\QueryHandlerInterface;
+use App\Users\Application\DTO\UserDTO;
+use App\Users\Domain\Repository\UserRepositoryInterface;
+
+class FindUserByEmailQueryHandler implements QueryHandlerInterface
+{
+    public function __construct(
+        private readonly UserRepositoryInterface $userRepository
+    ) {
+    }
+
+    public function __invoke(FindUserByEmailQuery $query): ?UserDTO
+    {
+        $user = $this
+            ->userRepository
+            ->findByEmail($query->email);
+
+        if (!$user) {
+            return null;
+        }
+
+        return UserDTO::fromEntity($user);
+    }
+}
