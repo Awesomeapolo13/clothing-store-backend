b682bb6e46a0c6a677c0a67c0256855fd3e3b574
ALogachev <aelog16@yandex.ru>
2022-12-11 17:48:19 +0300

    Task-5. JWT bundles configuration were finished

---
+++ src/Users/Domain/Entity/RefreshToken.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Users\Domain\Entity;
+
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+
+/**
+ * Класс рефреш токена
+ */
+class RefreshToken extends BaseRefreshToken
+{
+}
