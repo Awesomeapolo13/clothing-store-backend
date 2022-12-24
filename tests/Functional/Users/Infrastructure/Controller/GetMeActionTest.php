b0c0b531a461c4c0fab4f48b6c6a95b2caa5a941
ALogachev <aelog16@yandex.ru>
2022-12-24 18:33:35 +0300

    Task-5. Test for JWT and user get endpoint was added

---
+++ tests/Functional/Users/Infrastructure/Controller/GetMeActionTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Functional\Users\Infrastructure\Controller;
+
+use App\Tests\Tools\FixtureTools;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GetMeActionTest extends WebTestCase
+{
+    use FixtureTools;
+
+    public function test_get_me_action()
+    {
+        $client = static::createClient();
+        $user = $this->loadUserFixture();
+        // Выполняем запрос на аутентификацию
+        $client->request(
+            'POST',
+            '/api/auth/token/login',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword(),
+            ]),
+        );
+        // Получаем результат запроса
+        $data = json_decode(
+            $client
+                ->getResponse()
+                ->getContent(),
+            true
+        );
+        // Извлекаем из ответа токен
+        $client->getServerParameter(
+            'HTTP_AUTHORIZATION',
+            sprintf(
+                'Bearer %s',
+                $data['token']
+            )
+        );
+        // act Выполняем запрос получения информации об авторизованном ползователе
+        $client->request('GET', '/api/users/me');
+        /*
+         * assert Проверяем утверждение, что почта пользователя, полученного по эндоинту соответствует той,
+         * с помощью которой происходила авторизация
+         */
+        $data = json_decode(
+            $client
+                ->getResponse()
+                ->getContent(),
+            true
+        );
+        $this->assertEquals(
+            $user->getEmail(),
+            $data['email']
+        );
+    }
+}
--- tests/Tools/FakerTools.php
+++ tests/Tools/FakerTools.php
@@ -7,6 +7,9 @@ namespace App\Tests\Tools;
 use Faker\Factory;
 use Faker\Generator;
 
+/**
+ * Методы для работы с фейкером.
+ */
 trait FakerTools
 {
     /**
